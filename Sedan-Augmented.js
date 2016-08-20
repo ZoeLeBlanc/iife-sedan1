1525e2226c55e6bfabe1cb16b010044d6b992271
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-08-20 10:08:06 -0500

    first break

---
+++ Sedan-Augmented.js
@@ -0,0 +1,17 @@
+var Sedan = (function(newSedan) {
+	var max_occupancy = 6;
+	var current_occupancy = 0;
+
+	newSedan.getOccupancy = function() {
+		return current_occupancy;
+	}; 
+	newSedan.setOccupancy = function(newOccupancy){
+		if(newOccupancy <= max_occupancy) {
+			current_occupancy = newOccupancy;
+		} else {
+			throw "Cannot" + max_occupancy;
+		}
+	}
+	return newSedan;
+
+})(Sedan || {});
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//console.log("hi");
 console.log("Sedan ", Sedan);
 console.log("Color ", Sedan.getColor());
 console.log("Type ", Sedan.getType());
+console.log("Set Occupancy ", Sedan.setOccupancy(9));
+console.log("Get Occupancy ", Sedan.getOccupancy());
